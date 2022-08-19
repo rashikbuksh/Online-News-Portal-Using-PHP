a2421775abf55fa454d5c5a7655b069bf3f8807f
Rashik Buksh Rafsan <85847421+rashikbuksh@users.noreply.github.com>
2022-08-19 22:37:17 +0600

    login design

--- login.php
+++ login.php
@@ -54,19 +54,47 @@
     mysqli_close($conn);
 ?>
 
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Login</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-<html>
-    <head>
-        <title>Login Page</title>
-    </head>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <style>
+        body {
+        height: 100%;
+        background-image: linear-gradient(black, grey);
+        }
+    </style>
+  </head>
+  <body>
+    <div class="container h-100 d-flex justify-content-center vh-100">
+        <div class="col-sm-3"></div>
+        <div class="col-sm-6 jumbotron my-auto">
+            <h1 class="display-4" align="center">Online News Portal</h1>
+            <form action="login.php" method="post">
+            <div class="form-group">
+                <label for="username" style="color:#17a2b8;">Username</label>
+                <input type="text" class="form-control" id="username" name="username" placeholder="Enter username" >
+            </div>
+            <div class="form-group">
+                <label for="password" style="color:#17a2b8;">Password</label>
+                <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
+            </div>
+            <button type="submit" class="btn btn-outline-info" name="submit">Submit</button>
+            </form>
+        </div>
+        <div class="col-sm-3"></div>
+    </div>
 
-    <body>
-        <form method="POST">
-        Username: 
-        <input type="text" name="username" maxlength="50" />
-        Password: 
-        <input type="password" name="password" maxlength="50" />
-        <input type="submit" name="submit" value="login" />
-    </form>
-    </body>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
 </html>
