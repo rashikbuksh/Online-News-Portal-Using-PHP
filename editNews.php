2aeff729a0a68fb9e1dbb2cb9e755b8f63b6613e
Rashik Buksh Rafsan <85847421+rashikbuksh@users.noreply.github.com>
2022-09-03 21:23:45 +0600

    news edit selection added

--- editNews.php
+++ editNews.php
@@ -4,7 +4,7 @@
     $viewer = $_SESSION['access'];
     $userid = $_SESSION['userid'];
     if($loggedin == '1'){
-        echo "Welcome ".$_SESSION['username']. '<br><br>';
+        
     }
     else{
         header('location:login.php');
@@ -44,11 +44,154 @@
     }
 
 ?>
+<html lang="en">
+  <head>
+    <title>Homepage</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+    <body>
+<div role="navigation">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="homepage.php">Online News Portal</a>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="homepage.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <?php
+                        if($viewer == 'Viewer'){
+                            
+                        }
+                        else if($viewer == 'Admin'){
+                            echo '<a class="nav-link" href="addNews.php">Add News</a>';
+                        }
+                    ?>
+                </li>
+                </div>
+                </ul>
+                <form class="form-inline my-2 my-lg-0">
+                &nbsp;&nbsp;
+                <div class="dropdown show">
+                    <a class="btn btn-secondary dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <?php
+                    if($loggedin == '1'){
+                        echo "&nbsp".$_SESSION['username'];
+                    }
+                ?>
+                </a>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                        <a class="dropdown-item" href="login.php">Logout</a>
+                    </div>
+                </form>
+            </div>
+        </nav>
+    </div>
     <form method="post" enctype="multipart/form-data">
         Title: 
         <input type="text" name="title" id="title" maxlength="100" value="<?php echo $title ?>"/><br><br>
         News Type: 
-        <input type="text" name="type" id="type" maxlength="100" value="<?php echo $newstype ?>" /><br><br>
+        <select class="form-select" aria-label="Default select example" name="type">
+        <?php
+            switch($newstype)
+            {
+                case "World":
+                    echo "<option value='World' selected>World</option>
+                    <option value='International'>International</option>
+                    <option value='National'>National</option>
+                    <option value='Political'>Political</option>
+                    <option value='Lifestyle'>Lifestyle</option>
+                    <option value='Fashion'>Fashion</option>
+                    <option value='Gadget'>Gadget</option>
+                    <option value='Sports'>Sports</option>";
+                    break;
+
+                case "International":
+                    echo "<option value='International' selected>International</option>
+                    <option value='World'>World</option>
+                    <option value='National'>National</option>
+                    <option value='Political'>Political</option>
+                    <option value='Lifestyle'>Lifestyle</option>
+                    <option value='Fashion'>Fashion</option>
+                    <option value='Gadget'>Gadget</option>
+                    <option value='Sports'>Sports</option>";
+                    break;
+
+                case "National":
+                    echo "<option value='National' selected>National</option>
+                    <option value='World'>World</option>
+                    <option value='International'>International</option>
+                    <option value='Political'>Political</option>
+                    <option value='Lifestyle'>Lifestyle</option>
+                    <option value='Fashion'>Fashion</option>
+                    <option value='Gadget'>Gadget</option>
+                    <option value='Sports'>Sports</option>";
+                    break;
+                
+                case "Political":
+                    echo "<option value='Political' selected>Political</option>
+                    <option value='World'>World</option>
+                    <option value='International'>International</option>
+                    <option value='National'>National</option>
+                    <option value='Lifestyle'>Lifestyle</option>
+                    <option value='Fashion'>Fashion</option>
+                    <option value='Gadget'>Gadget</option>
+                    <option value='Sports'>Sports</option>";
+                    break;
+
+                case "Lifestyle":
+                    echo "<option value='Lifestyle' selected>Lifestyle</option>
+                    <option value='World'>World</option>
+                    <option value='International'>International</option>
+                    <option value='National'>National</option>
+                    <option value='Political'>Political</option>
+                    <option value='Fashion'>Fashion</option>
+                    <option value='Gadget'>Gadget</option>
+                    <option value='Sports'>Sports</option>";
+                    break;
+
+                case "Fashion":
+                    echo "<option value='Fashion' selected>Fashion</option>
+                    <option value='World'>World</option>
+                    <option value='International'>International</option>
+                    <option value='National'>National</option>
+                    <option value='Political'>Political</option>
+                    <option value='Lifestyle'>Lifestyle</option>
+                    <option value='Gadget'>Gadget</option>
+                    <option value='Sports'>Sports</option>";
+                    break;
+
+                case "Gadget":
+                    echo "<option value='Gadget' selected>Gadget</option>
+                    <option value='World'>World</option>
+                    <option value='International'>International</option>
+                    <option value='National'>National</option>
+                    <option value='Political'>Political</option>
+                    <option value='Lifestyle'>Lifestyle</option>
+                    <option value='Fashion'>Fashion</option>
+                    <option value='Sports'>Sports</option>";
+                    break;
+
+                case "Sports":
+                    echo "<option value='Sports' selected>Sports</option>
+                    <option value='World'>World</option>
+                    <option value='International'>International</option>
+                    <option value='National'>National</option>
+                    <option value='Political'>Political</option>
+                    <option value='Lifestyle'>Lifestyle</option>
+                    <option value='Fashion'>Fashion</option>
+                    <option value='Gadget'>Gadget</option>";
+                    break;
+            }
+            ?>
+            </select>
+        <br><br>
         Short Description: (optional)
         <input type="text" name="shortdescription" id="shortdescription" maxlength="255" values="<?php echo $shortdescription ?>"/><br><br>
         Main Image:
@@ -134,3 +277,7 @@
     mysqli_close($conn);
 ?>
 
+</body>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</html>
